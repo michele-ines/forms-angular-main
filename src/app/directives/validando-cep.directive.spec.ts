537ebbf8af5bd484c31e52577b8341b489727512
michele-ines <minesck@hotmail.com>
2025-06-04 17:05:41 -0300

    master: configurando directive validando-cep

--- src/app/directives/validando-cep.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidandoCepDirective } from './validando-cep.directive';
-
-describe('ValidandoCepDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidandoCepDirective();
-    expect(directive).toBeTruthy();
-  });
-});
